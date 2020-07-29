b72a26be1dec1c7df15825d76dfeecebbf3373b0
rpharr <rpharr@richmond.edu>
2020-07-29 12:07:56 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import { withAuthenticator } from 'aws-amplify-react'
+import Amplify, { Auth } from "aws-amplify";
+import aws_exports from "./aws-exports";
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -29,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default withAuthenticator(App, true);
+export default App;
